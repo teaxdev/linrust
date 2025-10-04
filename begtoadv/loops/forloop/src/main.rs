da2c0b7bd38f32ffe005cd4b0ccc3fe7b7a312ad
teaxdev <FloresAlex6904@gmail.com>
2025-10-04 10:24:24 -0500

    finish for loop section

--- begtoadv/loops/forloop/src/main.rs
+++ begtoadv/loops/forloop/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    // for i in 1..=10 {
+    //     println!("i = {}", i);
+    // }
+    
+    // for i in (1..=5).rev() {
+    //     println!("i = {}", i);
+    // };
+
+    // let numbers = vec![1, 2, 3, 4, 5];
+    // for n in numbers {
+    //     println!("{}", n);
+    // }
+
+    for i in 1..=10 {
+        if i % 2 == 0 {
+            continue;
+        }
+        println!("i = {}", i);
+        if i == 7 {
+            break;
+        }
+    }
 }
