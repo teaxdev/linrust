db713dffe29a1db4fb0670b1afa872eb71936511
teaxdev <FloresAlex6904@gmail.com>
2025-10-04 10:06:28 -0500

    start lin rust course

---
+++ begtoadv/intro/foo/src/main.rs
@@ -0,0 +1,49 @@
+// fn main() {
+//     let msg = "Name: alex, Weight: ";
+//     let weight = 120.0;
+
+//     let kilos = weight / 2.2;
+//     println!("{}{}", msg, weight)
+// }
+
+// fn mutabletest() {
+//     let mut message = String::from("Name: Bruce, Height: ");
+//     message.clear();
+//     let mut height = 190;
+//     height = 189;
+//     println!("{}{}", message, height)
+// }
+
+// fn main() {
+//     let proceed = true;
+//     if proceed {
+//         println!("Proceeding") 
+//     } else {
+//         println!("Not Proceeding")
+//     };
+// }
+
+// fn other() {
+//     let height = 190;
+//     if height > 180 {
+//         println!("Tall")
+//     } else if height > 170 {
+//         println!("average")
+//     } else {
+//         println!("short!")
+//     };
+// }
+
+// fn height() {
+//     let mut height = 190;
+//     height = height - 20;
+//     let result = if height > 180 {
+//         println!("Tall")
+//     } else if height > 170 {
+//         println!("average")
+//     } else {
+//         println!("short!")
+//     };
+
+//     println!("Result: {}", result);
+// }
---
+++ begtoadv/loops/bar/src/main.rs
@@ -0,0 +1,28 @@
+// fn main() {
+//     let mut x = 1;
+
+//     loop {
+//         println!("Hello, world!");
+//         x += 1;
+//         if x > 5 {
+//             break;
+//         }
+//     }
+// }
+
+// fn rewrew() {
+//     let maybe_number = Some(42);
+//     if let Some(number) = maybe_number {
+//         println!("num is {}", number);
+//     } else {
+//         println!("no num");
+//     }
+// }
+
+fn main() {
+    let mut i = 0;
+    while i < 5 {
+        println!("i = {}", i);
+        i += 1;
+    }
+}
