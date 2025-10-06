9a77bb47aba751e3b72c71de08132f0f0da3bedd
teaxdev <FloresAlex6904@gmail.com>
2025-10-05 23:03:27 -0500

    continue coursework

--- begtoadv/fun/args/src/main.rs
+++ begtoadv/fun/args/src/main.rs
@@ -6,8 +6,18 @@ fn sum(numbers: &[i32]) -> i32  {
     result
 }
 
+fn panicahhh(numbers: Vec<i32>) {
+    for num in numbers {
+        if num < 0 {
+            panic!("Negative number found");
+        }
+        println!("Number: {}", num)
+    }
+}
+
 fn main() {
     let numbers = [1, 2, 3, 4, 5];
     let result = sum(&numbers);
     println!("the sum is {}", result);
+    panicahhh(vec![1, 2, 3, 4, -5]);
 }
